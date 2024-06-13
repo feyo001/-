1a77b60d3133a6021b9ae3d36cf4a1e885e09333
feyo001 <56681734+feyo001@users.noreply.github.com>
2024-06-13 20:18:53 +0100

    Update Report.py

--- pages/Report.py
+++ pages/Report.py
@@ -1,5 +1,5 @@
-# from .Analytics import get_sheet_data
-import Analytics
+from pages.Analytics import get_sheet_data
+# import Analytics
 expenses = get_sheet_data("Expenses")
 expenses_df= expences_data_wrangle(expenses)
 st.write(expenses_df)
