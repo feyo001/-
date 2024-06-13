f4f394d5c0ddbe945d1f0ea1bc4a72ede7cb8855
Eigbe Francis <ehizielen.eigbe@gmail.com>
2024-06-13 22:59:44 +0100

    Entering details for the generated report.

--- pages/Report.py
+++ pages/Report.py
@@ -11,32 +11,28 @@ sales_df = data_wrangle(sales)
 expenses = get_sheet_data("Expenses")
 expenses_df= expences_data_wrangle(expenses)
 
-# st.write(sales_df)
-# st.write(expenses_df)
-
-
 
 df = pd.DataFrame(sales_df)
 # st.write(df)
 df['DATE'] = pd.to_datetime(df['DATE'])
-st.write(df.head())
+# st.write(df.head())
 
-# # Get date input from the user
-# selected_date = st.date_input("Select Date")
+# Get date input from the user
+selected_date = st.date_input("Select Date")
 
-# # Filter the DataFrame by the selected date
-# filtered_df = df[df['Date'] == pd.to_datetime(selected_date)]
+# Filter the DataFrame by the selected date
+filtered_df = df[df['DATE'] == pd.to_datetime(selected_date)]
+st.write(filtered_df.head())
 
-
-# # Generate the report
-# def generate_report(date, df):
-#     report = f"Date: {date.strftime('%d/%m/%Y')}\n"
-#     report += "Sale:\n"
-#     report += "Purchases:\n"
+# Generate the report
+def generate_report(date, df):
+    report = f"Date: {date.strftime('%d/%m/%Y')}\n"
+    report += "Sale:\n"
+    report += "Purchases:\n"
 #     purchases = df[df['Category'] == 'Purchases']
 #     for index, row in purchases.iterrows():
 #         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Exp:\n"
+# #     report += "Exp:\n"
 #     expenses = df[df['Category'] == 'Exp']
 #     for index, row in expenses.iterrows():
 #         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
@@ -47,13 +43,13 @@ st.write(df.head())
 #     report += "Contribution:\n"
 #     report += "Pepvic: \n"
 #     report += "mama: \n"
-#     return report
+    return report
 
-# report = generate_report(selected_date, filtered_df)
+report = generate_report(selected_date, filtered_df)
 
 
-# # Display the report
-# st.text_area("Printable Report", report, height=400)
+# Display the report
+st.text_area("Printable Report", report, height=400)
 ################################################################################
 
 # df = pd.DataFrame(data)
