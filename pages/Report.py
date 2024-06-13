cfba7b9d520c186b5e5e8b7d866643aaeb246856
feyo001 <56681734+feyo001@users.noreply.github.com>
2024-06-13 20:34:36 +0100

    Getting sales data sheet

--- pages/Report.py
+++ pages/Report.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from pages.Analytics import get_sheet_data
 from pages.Analytics import expences_data_wrangle
+
+
+
+sales = get_sheet_data("Sales")
+sales_df = data_wrangle(sales)
 expenses = get_sheet_data("Expenses")
 expenses_df= expences_data_wrangle(expenses)
 st.write(expenses_df)
