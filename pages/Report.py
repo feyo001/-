571d5f5349cf5048c1adf26ce2c20f69a6be070c
feyo001 <56681734+feyo001@users.noreply.github.com>
2024-06-13 21:53:06 +0100

    Display sales dataframe

--- pages/Report.py
+++ pages/Report.py
@@ -9,4 +9,6 @@ sales = get_sheet_data("Sales")
 sales_df = data_wrangle(sales)
 expenses = get_sheet_data("Expenses")
 expenses_df= expences_data_wrangle(expenses)
+
+st.write(sales_df)
 st.write(expenses_df)
