5f35e2780e7c30ad8b7710a9d42a33fdee808a05
Eigbe Francis <ehizielen.eigbe@gmail.com>
2024-06-14 15:50:22 +0100

    Show expense table

--- pages/Report.py
+++ pages/Report.py
@@ -28,82 +28,24 @@ st.write(filtered_df.head())
 
 filtered_expenses_df = expenses_df[['Date','Item','Description','Amount']]
 filtered_expenses_df = filtered_expenses_df[filtered_expenses_df['Date'] == pd.to_datetime(selected_date)]
-st.write(filtered_expenses_df.head())
+# comfort_df = filtered_expenses_df.query('Item == "Comfort"')
+# peter_df = filtered_expenses_df.query('Item == "Peter"')
+
 
 # Generate the report
 def generate_report(date, df):
     report = f"Date: {date.strftime('%d/%m/%Y')}\n"
     report += f"Sales: ₦{df['PRICE'].sum():,.2f}\n"
     report += f"Expense(s): ₦{filtered_expenses_df['Amount'].sum():,.2f}\n"
-#   report += "Purchases:\n"
-#     # purchases = df[df['Category'] == 'Purchases']
-# #     report += "Exp:\n"
-#     expenses = df[df['Category'] == 'Exp']
-#     for index, row in expenses.iterrows():
-#         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Miscellaneous:\n"
-#     miscellaneous = df[df['Category'] == 'Miscellaneous']
-#     for index, row in miscellaneous.iterrows():
-#         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Contribution:\n"
-#     report += "Pepvic: \n"
-#     report += "mama: \n"
+
     return report
 
 report = generate_report(selected_date, filtered_df)
-
-#def generate_report1(date, df):
-#    for index, row in filtered_expenses_df.iterrows():
-#        report1=""
-#        report1 += f"{row['Item']}: ₦{row['Amount']}\n"
-#    return report1
-
-def generate_report1(date, df):
-  report1 = [f"{row['Item']}: ₦{row['Amount']}\n" for index, row in df.iterrows()]
-  return "\n".join(report1)  # Join the list items into a string with newlines
-    
-        
-report1 = generate_report1(selected_date, filtered_expenses_df)
-
+ 
 # Display the report
-st.text_area("Printable Report", report, height=150)
-################################################################################
-st.text_area("Expense Report", report1, height=150)
-
-################################################################################
+st.text_area("Printable Report", report, height=120)
 
 
-# df = pd.DataFrame(data)
-# df['Date'] = pd.to_datetime(df['Date'])
+# Expense breakdown Table
+st.write(filtered_expenses_df[['Item','Description','Amount']].reset_index(drop=True))
 
-# # Get date input from the user
-# selected_date = st.date_input("Select Date")
-
-# # Filter the DataFrame by the selected date
-# filtered_df = df[df['Date'] == pd.to_datetime(selected_date)]
-
-# # Generate the report
-# def generate_report(date, df):
-#     report = f"Date: {date.strftime('%d/%m/%Y')}\n"
-#     report += "Sale:\n"
-#     report += "Purchases:\n"
-#     purchases = df[df['Category'] == 'Purchases']
-#     for index, row in purchases.iterrows():
-#         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Exp:\n"
-#     expenses = df[df['Category'] == 'Exp']
-#     for index, row in expenses.iterrows():
-#         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Miscellaneous:\n"
-#     miscellaneous = df[df['Category'] == 'Miscellaneous']
-#     for index, row in miscellaneous.iterrows():
-#         report += f"{row['Details']}: N{row['Amount']} by {row['Person']}\n"
-#     report += "Contribution:\n"
-#     report += "Pepvic: \n"
-#     report += "mama: \n"
-#     return report
-
-# report = generate_report(selected_date, filtered_df)
-
-# # Display the report
-# st.text_area("Printable Report", report, height=400)
